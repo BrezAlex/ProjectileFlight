65cd75d5669d7cd3b79ab95e5c8cb942136a5652
Александр <sasha.brezin7262@gmail.com>
2021-12-11 19:16:15 +0300

    Добавлен график (тест)

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hi")
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = (0, 10, 50)
+y = x
+
+
+plt.title("Зависимость x(y)")
+plt.xlabel("x")
+plt.xlabel("y")
+plt.grid()
+plt.plot(x, y)
+plt.show()
