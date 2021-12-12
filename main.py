5799b3fb48151a62ca7b3bd027c993be0e3e7018
Александр <sasha.brezin7262@gmail.com>
2021-12-12 15:46:34 +0300

    Падение в низ

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = (0, 10, 50)
-y = x
+g = 9.81
+v0 = 10
+alpha = 45
+x0 = 0
+y0 = 0
+t = 1
+dt = input("Введите значение dt: ")
+
+x = []
+y = []
+
+while y <= 0:
 
 
-plt.title("Зависимость x(y)")
-plt.xlabel("x")
-plt.xlabel("y")
-plt.grid()
-plt.plot(x, y)
-plt.show()
+
+# vx = []
+# vy = []
+#
+# while t <= 10:
+#     t += float(dt)
+#     vx_value = v0 * np.cos(alpha)
+#     vy_value = v0 * np.sin(alpha) - g * t
+#     vx.append(vx_value)
+#     vy.append(vy_value)
+#
+# v = np.sqrt(vx_value ** 2 + vy_value ** 2)
+# print(v)
+# print(vx)
+# print(vy)
+#
+# plt.plot(vx, vy)
+# plt.show()
